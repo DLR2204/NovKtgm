342e5fa41dc792db20a8271976a112a08acfe756
DLR2204 <n130488@rguktn.ac.in>
2024-11-27 08:00:11 +0530

    Add files via upload

---
+++ CoreJava/intro/Example.java
@@ -0,0 +1,33 @@
+package intro;
+
+public class Example {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hi Everyone");
+		
+		short hello ;  //declaration
+		
+		byte number = 120;  //initiallization
+		
+		int value = 334;
+		
+		char name = 'f';
+		
+		float pi = 3.14f;
+		
+		String name1 = "apple";
+		
+		System.out.println(number);
+		
+		System.out.println(value);
+		
+		System.out.println(name);
+		
+		System.out.println(pi);
+		
+		System.out.println(name1);
+
+	}
+
+}
---
+++ CoreJava/intro/VariablesEx.java
@@ -0,0 +1,16 @@
+package intro;
+
+
+
+
+public class VariablesEx {
+	
+	String name = "GlobalVariable"; //GlobalVr
+	
+	public static void main(String[] args) {
+		
+		String name1 = "LocalVariable";
+		
+	}
+
+}
