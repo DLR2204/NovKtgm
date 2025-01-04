a14d674f7200d9dec823a8f3f6e5f43e51df5f1c
DLR2204 <n130488@rguktn.ac.in>
2025-01-04 08:36:33 +0530

    Add files via upload

---
+++ spring/SpringBoot/service/Services.java
@@ -0,0 +1,14 @@
+package com.service;
+
+import com.entity.Employee;
+
+public interface Services {
+
+	public String addEmp(Employee emp);
+
+	public String updateEmp(Employee emp);
+
+	public String deleteEmp(int empId);
+
+	public Employee getEmp(int empId);
+}
